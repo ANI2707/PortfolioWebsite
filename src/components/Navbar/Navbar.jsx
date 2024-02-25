5b339ba91fb436777cdcc0a51973812b2b70eb56
[Aniket Bhosale] <[adbhosale2707]>
2024-02-25 15:55:01 +0530

    Navbar design completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
 import "./Navbar.css";
 import { HiOutlineMenu } from "react-icons/hi";
+import { MdOutlineClose } from "react-icons/md";
 
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
-  
-
 
   return (
     <>
       <nav className="navbar">
         {/* Desktop view navigation */}
-        
+
         <ul className="nav-list">
           <li>
             <a href="#">Home</a>
@@ -31,8 +30,8 @@ const Navbar = () => {
         </ul>
 
         {/* Mobile view icon */}
-        <div className="mobile-menu"  onClick={()=>setShowNav(!showNav)} >
-          <HiOutlineMenu />
+        <div className="mobile-menu" onClick={() => setShowNav(!showNav)}>
+          {showNav ? <MdOutlineClose style={{ fontSize: "1.5rem" }}/> : <HiOutlineMenu style={{ fontSize: "1.5rem" }}/>}
         </div>
       </nav>
       {/* Mobile view navigation */}
