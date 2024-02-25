e99b4c57ba00865ed695c59993ecb5f6ed130f62
[Aniket Bhosale] <[adbhosale2707]>
2024-02-25 15:37:41 +0530

    Humburger menu added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import "./Navbar.css";
+import { HiOutlineMenu } from "react-icons/hi";
 
 const Navbar = () => {
-  const [showNav,setShowNav]=useState(false);
+  const [showNav, setShowNav] = useState(false);
+  
+
+
   return (
     <>
       <nav className="navbar">
-        
+        {/* Desktop view navigation */}
         
         <ul className="nav-list">
           <li>
@@ -25,7 +29,32 @@ const Navbar = () => {
             <button>Resume</button>
           </li>
         </ul>
+
+        {/* Mobile view icon */}
+        <div className="mobile-menu"  onClick={()=>setShowNav(!showNav)} >
+          <HiOutlineMenu />
+        </div>
       </nav>
+      {/* Mobile view navigation */}
+      {showNav && (
+        <ul className="mobile-nav-list">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Experience</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact me</a>
+          </li>
+          <li>
+            <button>Resume</button>
+          </li>
+        </ul>
+      )}
     </>
   );
 };
