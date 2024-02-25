937dcca9eee41d25740b8cd076792dafde0ab867
[Aniket Bhosale] <[adbhosale2707]>
2024-02-25 11:29:36 +0530

    navbar structure added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [showNav,setShowNav]=useState(false);
+  return (
+    <>
+      <nav className="navbar">
+        
+        
+        <ul className="nav-list">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Experience</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Resume</a>
+          </li>
+          <li>
+            <a href="#">Contact me</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
