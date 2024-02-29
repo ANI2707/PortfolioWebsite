18fd4a633b8d40992514f76fb6a63cc9c8044097
[Aniket Bhosale] <[adbhosale2707]>
2024-02-29 16:36:01 +0530

    form added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,12 +5,43 @@ const Contact = () => {
   return (
     <>
       <div className="main">
-        <div className="content-right">
+        <div className="content-left">
           <img src={formAnimate} alt="form image" className="contact-img" />
         </div>
-        <div className="content-left">
+        <div className="content-right">
           {/* form */}
-          
+          <div className="contact-form-header">Get in Touch</div>
+          <div className="form-container">
+            {/* form */}
+            <form>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Your Name"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email Address"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="message"
+                  id="message"
+                  placeholder="Message"
+                />
+              </div>
+
+              <button>Send</button>
+            </form>
+          </div>
         </div>
       </div>
     </>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Footer.css';
+import { FaRegHeart } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
+import { SiGithub } from "react-icons/si";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div>
-        <p> Made with 🧡 by Aniket Bhosale</p>
+      <div className='footer-left'>
+        <p> Made with <FaRegHeart/> by Aniket Bhosale</p>
+      </div>
+      <div className='footer-middle'>
+        <span>Copyright © 2024 Aniket</span>
+      </div>
+      <div className='footer-social-icons'>
+        <CiLinkedin/>
+        <SiGithub/>
+
       </div>
     </div>
   )
