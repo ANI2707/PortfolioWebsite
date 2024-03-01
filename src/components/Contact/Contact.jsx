c7f4904546cb130591b1d496ab124246ea7c534a
Aniket <108017771+ANI2707@users.noreply.github.com>
2024-03-01 08:36:05 +0000

    form backend completed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import "./Contact.css";
 import formAnimate from "../../assets/forms-animate.svg";
+import emailjs from '@emailjs/browser';
+import { useRef } from "react";
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_4l66qwn', 'template_5k073v1', form.current, {
+        publicKey: 'I82bBPJs7uLhLd73v',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+      form.current.reset();
+  }
+  
   return (
     <>
       <div className="main">
@@ -13,21 +36,23 @@ const Contact = () => {
           <div className="contact-form-header">Get in Touch</div>
           <div className="form-container">
             {/* form */}
-            <form>
+            <form ref={form} onSubmit={sendEmail}>
               <div className="form-group">
                 <input
                   type="text"
-                  name="name"
+                  name="from_name"
                   id="name"
                   placeholder="Your Name"
+                  required
                 />
               </div>
               <div className="form-group">
                 <input
                   type="email"
-                  name="email"
+                  name="from_email"
                   id="email"
                   placeholder="Email Address"
+                  required
                 />
               </div>
               <div className="form-group">
@@ -36,6 +61,7 @@ const Contact = () => {
                   name="message"
                   id="message"
                   placeholder="Message"
+                  required
                 />
               </div>
 
