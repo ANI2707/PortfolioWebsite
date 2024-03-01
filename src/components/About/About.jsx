b1fd4c89504e62219dc1b9c9a949ba209714a68f
Aniket <108017771+ANI2707@users.noreply.github.com>
2024-03-01 10:41:44 +0000

    about changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import './About.css'
 const About = () => {
   return (
-    <div>About</div>
+    <>
+
+      <div className='about'>
+        <div className='about-img'>
+          <img src="" alt="personal-img" />
+        </div>
+        <div className='about-info'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non vel aperiam distinctio magnam maxime minus, molestiae suscipit animi quibusdam earum dicta quia dolor consequuntur id eveniet voluptas? Sapiente, eos. Atque!</p>
+        </div>
+        <div className='education'>
+          <div className='education-header'>
+            Education
+          </div>
+          
+            
+
+        </div>
+
+
+      </div>
+
+    </>
   )
 }
 
